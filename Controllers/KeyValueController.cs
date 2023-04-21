e0244adf144d7243839ede34e8ec81f0e36fac86
NageshDhone <nageshdhone93@gmail.com>
2023-04-21 18:20:21 +0530

    Add project files.

---
+++ Controllers/KeyValueController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApi.Data;
+
+namespace MyApi.Controllers
+{
+    [ApiController]
+    [Route("api/[KeyValue]")]
+    public class KeyValueController : Controller
+    {
+        private readonly KeyValueAPIDbContext dbcontext;
+
+        public KeyValueController(KeyValueAPIDbContext dbContext)
+        {
+            this.dbcontext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetKeyValue()
+        {
+           return Ok  (dbcontext.keyValues.ToList());
+           // return View();
+        }
+    }
+}
---
+++ Data/KeyValueAPIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApi.Models;
+
+namespace MyApi.Data
+{
+    public class KeyValueAPIDbContext : DbContext
+    {
+        protected KeyValueAPIDbContext(DbContextOptions options):base(options) 
+        {
+        }
+        public DbSet<KeyValue> keyValues { get; set; }
+    }
+}
